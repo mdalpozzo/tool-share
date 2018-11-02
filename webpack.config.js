fb1ba176ba0d35bd8d6eda313bfe2133e49e5913
Marlin Dalpozzo <marlin.dalpozzo@gmail.com>
2018-11-02 02:54:50 -0700

    adds client webpack.config.js. comments out the root webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,41 @@
-const path = require('path');
+// const path = require('path');
 
-const SRC_DIR = path.join(__dirname, '/client/src');
-const DIST_DIR = path.join(__dirname, '/client/public');
+// const SRC_DIR = path.join(__dirname, '/client/src');
+// const DIST_DIR = path.join(__dirname, '/client/public');
 
-module.exports = {
-  mode: 'development',
-  entry: [`${SRC_DIR}/App.jsx`],
-  output: {
-    path: DIST_DIR,
-    // publicPath: '/',
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(png|jpg|gif)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {},
-          },
-        ],
-      },
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'env', 'stage-0'],
-          plugins: ['transform-class-properties'],
-        },
-      },
-      {
-        test: /\.(s*)css$/,
-        exclude: /node_modules/,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
-};
+// module.exports = {
+//   mode: 'development',
+//   entry: [`${SRC_DIR}/App.jsx`],
+//   output: {
+//     path: DIST_DIR,
+//     // publicPath: '/',
+//     filename: 'bundle.js',
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.(png|jpg|gif)$/,
+//         use: [
+//           {
+//             loader: 'file-loader',
+//             options: {},
+//           },
+//         ],
+//       },
+//       {
+//         test: /\.(js|jsx)$/,
+//         exclude: /node_modules/,
+//         loader: 'babel-loader',
+//         query: {
+//           presets: ['react', 'env', 'stage-0'],
+//           plugins: ['transform-class-properties'],
+//         },
+//       },
+//       {
+//         test: /\.(s*)css$/,
+//         exclude: /node_modules/,
+//         use: ['style-loader', 'css-loader'],
+//       },
+//     ],
+//   },
+// };
