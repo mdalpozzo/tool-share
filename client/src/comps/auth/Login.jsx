39fb0970176f3d173c5f0795e26bacf4ff2dc74b
Marlin Dalpozzo <marlin.dalpozzo@gmail.com>
2018-11-02 03:28:56 -0700

    adds tool photo to pages

--- client/src/comps/auth/Login.jsx
+++ client/src/comps/auth/Login.jsx
@@ -50,30 +50,32 @@ class Login extends Component {
 
     return (
       <div className="login">
-        <div className="container">
+        <div className="container-fluid">
           <div className="row">
-            <div className="col-md-8 m-auto">
-              <h1 className="display-4 text-center">Log In</h1>
-              <p className="lead text-center">Sign in to your Tool Share account</p>
-              <form onSubmit={this.onSubmit}>
-                <TextFieldGroup
-                  placeholder="Email Address"
-                  name="email"
-                  type="email"
-                  value={this.state.email}
-                  onChange={this.onChange}
-                  error={errors.email}
-                />
-                <TextFieldGroup
-                  placeholder="Password"
-                  name="password"
-                  type="password"
-                  value={this.state.password}
-                  onChange={this.onChange}
-                  error={errors.password}
-                />
-                <input type="submit" className="btn btn-info btn-block mt-4" />
-              </form>
+            <div className="container-fluid login-inner">
+              <div className="col-md-8 m-auto">
+                <h1 className="display-4 text-center">Log In</h1>
+                <p className="lead text-center">Sign in to your Tool Share account</p>
+                <form onSubmit={this.onSubmit}>
+                  <TextFieldGroup
+                    placeholder="Email Address"
+                    name="email"
+                    type="email"
+                    value={this.state.email}
+                    onChange={this.onChange}
+                    error={errors.email}
+                  />
+                  <TextFieldGroup
+                    placeholder="Password"
+                    name="password"
+                    type="password"
+                    value={this.state.password}
+                    onChange={this.onChange}
+                    error={errors.password}
+                  />
+                  <input type="submit" className="btn btn-info btn-block mt-4" />
+                </form>
+              </div>
             </div>
           </div>
         </div>
