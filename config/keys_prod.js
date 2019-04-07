0ad4c8ad853aeb17440d66e08e6f797459ec6728
Marlin Dalpozzo <marlin.dalpozzo@gmail.com>
2019-04-07 10:46:39 -0700

    sets up env variables for cloudinary image storage

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,9 @@
 module.exports = {
   mongoURI: process.env.DB_HOST,
   secretKey: process.env.SECRET_KEY,
+  cloudinary: {
+    name: process.env.CLOUDINARY_NAME,
+    key: process.env.CLOUDINARY_KEY,
+    secret: process.env.CLOUDINARY_SECRET
+  }
 };
